66ab8403fcf7ab31e17a95210f1862c1f45e8e09
Walden Yan <waldenyan20@gmail.com>
2022-11-13 04:13:06 -0500

    updated extension

--- frontend/components/Actions.tsx
+++ frontend/components/Actions.tsx
@@ -13,8 +13,29 @@ const Actions = () => {
     } = useContext(TabContext);
 
     return userType == UserType.BUYER ? (
-        <div>
-            <Request />
+        <div className={"container mx-auto text-center"}>
+            <div className={"w-1/2 mx-auto"}>
+                <h1 className={"my-4 font-bold"}>Step 1: Add credits</h1>
+                <p>
+                    If your account credit balance is low (below 150 TRX), you won't have enough
+                    to start a new rental. You are required to have a balance that is enough to cover
+                    24 hours of usage even if you don't use the account for the entire time.<br/>
+
+                    Go add credits if you need to.
+                </p>
+
+                <h1 className={"my-4 font-bold"}>Step 2: Open the extension</h1>
+                <p>
+                    The KeyStream extension lets you rent accounts for 24 hours at a time.
+                </p>
+
+                <h1 className={"my-4 font-bold"}>Step 3: Click "Rent a Login"</h1>
+                <p>
+                    You will then wait until a seller accepts your request. Once they do, you will
+                    automatically be logged into the account and can use it for up to 24 hours. You may
+                    also end your rental early if you wish and you willonly be charged for the time you used.
+                </p>
+            </div>
         </div>
 
     ) : (
