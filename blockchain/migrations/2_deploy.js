f7103c329b950df1112ed88ea99311bbe79fe8b8
Walden Yan <waldenyan20@gmail.com>
2022-11-12 19:27:49 -0500

    abi

--- blockchain/migrations/2_deploy.js
+++ blockchain/migrations/2_deploy.js
@@ -1,5 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
 const KeyStream = artifacts.require("./KeyStream.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(KeyStream);
+module.exports = async function(deployer) {
+  const result = deployer.deploy(KeyStream);
+  const res = await result.await;
+  const abi = res.abi;
+
+  // Write abi as JSON to '../extension/src/KeyStreamABI.json'
+  const filePath = path.join(__dirname, '../../extension/src/KeyStreamABI.json');
+  fs.writeFileSync(filePath, JSON.stringify(abi, null, 2));
+
+  console.log(res.address);
 };
